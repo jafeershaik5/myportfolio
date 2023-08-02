4141b4c2aaaa436035c22cb5777e55e6e31dae61
jafeershaik5 <jafeershaik5@gmail.com>
2023-08-02 12:59:31 +0530

    sample navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
 
 function App() {
     return (
-        <div>
-           protfolio website
-        </div>
+        <>
+            <Navbar/>
+        </>
     );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='Navbar-container'>
+            <div>SJ</div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Resume</li>
+                    <li>Contact Me</li>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
